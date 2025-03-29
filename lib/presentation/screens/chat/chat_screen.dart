8aa1c559ef43e54b44cdcfb36538c79369c736a0
Hector Illescas <hector.gpillescas@gmail.com>
2025-03-29 00:20:30 -0600

    ListView_and_Messages

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage("https://th.bing.com/th/id/OIP.BNTaNzja4b9B-_qaAV7XDAAAAA?rs=1&pid=ImgDetMain"),
+          ),
+        ),
+        title: Text('Maverick'),
+        ),
+        body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal:10),
+        child: Column(
+          children: [
+            Expanded(child:ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+              return ( index % 2 ==0 )
+              ? const HerMessageBubble()
+              : const MyMessageBubble();
+
+            })),
+            Text("Mundo")
+          ],
+        ),
+      ),
+    );
+  }
+}
