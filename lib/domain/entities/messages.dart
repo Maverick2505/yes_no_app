95a0322b229ec1c6ccc7421e51b564fd9de9e393
Hector Illescas <hector.gpillescas@gmail.com>
2025-04-07 12:58:47 -0600

    Creating ChatProvider

---
+++ lib/domain/entities/messages.dart
@@ -0,0 +1,13 @@
+enum FromWho { me, him}
+
+//Identidad atómica: La unidad más pequeña
+class Message{
+  final String text;
+  final String? imageUrl;
+  final FromWho fromWho;
+
+  Message({
+    required this.text, 
+    this.imageUrl, 
+    required this.fromWho});
+}
